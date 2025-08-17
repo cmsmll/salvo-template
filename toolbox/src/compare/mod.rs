0ff4630b282979764c2199a2e54f19685a98a0d8
Mll <1939356771@qq.com>
2025-08-17 16:41:47 +0800

    feat: 实现部分快速比较Trait

---
+++ toolbox/src/compare/mod.rs
@@ -0,0 +1,9 @@
+pub mod str;
+
+pub fn always_true(_: &str) -> bool {
+    true
+}
+
+pub fn always_false(_: &str) -> bool {
+    false
+}
---
+++ toolbox/src/compare/str.rs
@@ -0,0 +1,47 @@
+use std::{collections::HashSet, sync::Arc};
+
+pub trait CompareStr: Clone {
+    fn compare(&self, uri: &str) -> bool;
+}
+
+impl<T: Fn(&str) -> bool + Clone> CompareStr for T {
+    fn compare(&self, uri: &str) -> bool {
+        self(uri)
+    }
+}
+
+impl CompareStr for &str {
+    fn compare(&self, uri: &str) -> bool {
+        uri.contains(self)
+    }
+}
+
+impl CompareStr for Arc<String> {
+    fn compare(&self, uri: &str) -> bool {
+        uri.contains(self.as_ref())
+    }
+}
+
+impl<const N: usize> CompareStr for &'static [&str; N] {
+    fn compare(&self, uri: &str) -> bool {
+        self.iter().any(|a| uri.contains(a))
+    }
+}
+
+impl CompareStr for Arc<Vec<String>> {
+    fn compare(&self, uri: &str) -> bool {
+        self.iter().any(|a| uri.contains(a))
+    }
+}
+
+impl CompareStr for Arc<HashSet<&'static str>> {
+    fn compare(&self, uri: &str) -> bool {
+        self.contains(uri)
+    }
+}
+
+impl CompareStr for Arc<HashSet<String>> {
+    fn compare(&self, uri: &str) -> bool {
+        self.contains(uri)
+    }
+}
