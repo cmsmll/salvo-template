eb15e157348126046c2fd117a6566cf18e541cc6
Mll <1939356771@qq.com>
2025-08-17 16:59:15 +0800

    fix: 调整代码布局并重新导出

--- toolbox/src/validator/extractor.rs
+++ toolbox/src/validator/extractor.rs
@@ -1,28 +1,66 @@
-use std::sync::LazyLock;
+use std::{io::Write, sync::LazyLock};
 
 use derive_more::derive::{Deref, DerefMut};
 use salvo::{
+    Extractible, Request,
     http::{
-        headers::{ContentType, HeaderMapExt},
         HeaderMap,
+        headers::{ContentType, HeaderMapExt},
     },
-    Extractible,
 };
 use serde::Deserialize;
 use validator::Validate;
 
-use crate::global::METADATE;
+use crate::{global::METADATE, reject, resp::Res};
 
-use super::{parse_form_validate, parse_json_validate, parse_path_validate, parse_query_validate};
+/// 数据验证
+pub fn validate(data: impl Validate) -> Result<(), Res> {
+    if let Err(err) = data.validate() {
+        let mut msg: Vec<u8> = Vec::new();
+        write!(msg, "数据验证失败: ")?;
+        for (key, value) in err.field_errors() {
+            write!(msg, "{key}<")?;
+            for field in value {
+                write!(msg, "{}, ", field.code)?
+            }
+            msg.truncate(msg.len() - 2);
+            msg.extend(b">; ");
+        }
+        msg.pop();
+        return reject!(422, unsafe { String::from_utf8_unchecked(msg) });
+    }
+    Ok(())
+}
+
+pub async fn parse_json_validate<T: Validate + for<'a> Deserialize<'a>>(req: &mut Request) -> Result<T, Res> {
+    let data: T = req.parse_json().await?;
+    validate(&data)?;
+    Ok(data)
+}
+
+pub async fn parse_form_validate<T: Validate + for<'a> Deserialize<'a>>(req: &mut Request) -> Result<T, Res> {
+    let data: T = req.parse_form().await?;
+    validate(&data)?;
+    Ok(data)
+}
+
+pub async fn parse_path_validate<T: Validate + for<'a> Deserialize<'a>>(req: &mut Request) -> Result<T, Res> {
+    let data: T = req.parse_params()?;
+    validate(&data)?;
+    Ok(data)
+}
+
+pub async fn parse_query_validate<T: Validate + for<'a> Deserialize<'a>>(req: &mut Request) -> Result<T, Res> {
+    let data: T = req.parse_queries()?;
+    validate(&data)?;
+    Ok(data)
+}
 
 static JSON: LazyLock<ContentType> = LazyLock::new(ContentType::json);
 
 /// 判断 json 请求头
 pub fn is_json_content(headers: &HeaderMap) -> bool {
-    headers
-        .typed_get::<ContentType>()
-        .map(|t| t == *JSON)
-        .unwrap_or(false)
+    headers.typed_get::<ContentType>().map(|t| t == *JSON).unwrap_or(false)
 }
 
 /// 提取 Json 数据并校验
@@ -41,7 +79,7 @@ impl<'ex, T: Validate + for<'de> Deserialize<'de>> Extractible<'ex> for VJson<T>
     }
 }
 
-/// 提取 Form 数据并校验``
+/// 提取 Form 数据并校验
 #[derive(Debug, Deref, DerefMut)]
 pub struct VForm<T: Validate>(pub T);
 
@@ -52,10 +90,7 @@ impl<'ex, T: Validate + for<'de> Deserialize<'de>> Extractible<'ex> for VForm<T>
 
     async fn extract(
         req: &'ex mut salvo::Request,
-    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static>
-    where
-        Self: Sized,
-    {
+    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static> {
         parse_form_validate(req).await.map(Self)
     }
 }
@@ -71,10 +106,7 @@ impl<'ex, T: Validate + for<'de> Deserialize<'de>> Extractible<'ex> for VPath<T>
 
     async fn extract(
         req: &'ex mut salvo::Request,
-    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static>
-    where
-        Self: Sized,
-    {
+    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static> {
         parse_path_validate(req).await.map(Self)
     }
 }
@@ -90,10 +122,7 @@ impl<'ex, T: Validate + for<'de> Deserialize<'de>> Extractible<'ex> for VQuery<T
 
     async fn extract(
         req: &'ex mut salvo::Request,
-    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static>
-    where
-        Self: Sized,
-    {
+    ) -> Result<Self, impl salvo::Writer + Send + std::fmt::Debug + 'static> {
         parse_query_validate(req).await.map(Self)
     }
 }
